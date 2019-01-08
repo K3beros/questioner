92ca6a703ab09dcd119d01420c0d4e6afdba04f1
Davis <dave.slim9@gmail.com>
2019-01-08 12:02:35 +0100

    Started with questions endpoint

---
+++ api/v1/controllers/questionController.js
@@ -0,0 +1,23 @@
+import questions from '../models/questionModel';
+import meetups from '../models/meetupModel'
+;
+class QuestionControllers {
+  static createQuestions(req, res) {
+    if (!req.body.createdby && !req.body.meetupid && !req.body.title && !req.body.questBody) {
+      return res.status(400).send({ status: 400, error: 'Missing field' });
+    }
+    let meetupid = meetups.find((element) => {
+      if (element.id === req.body.meetupid) { 
+        return element.id;
+      }
+    });
+    if (meetups.length < 1 || req.body.meetupid !== meetupid) {
+      return res.status(400).send({ status: 400, error: 'Invalid input' });
+    }
+    let {
+      createdby, meetupid, title, questBody
+ } = req.body;
+  }
+}
+
+export default QuestionControllers;
